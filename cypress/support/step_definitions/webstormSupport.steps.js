40952454703d59b6a5c2b1bb2db28ca57737926a
Kay van Bree <kayvanbree@gmail.com>
2018-06-27 09:47:04 +0200

    fix #56 Add test for Webstorm support

---
+++ cypress/support/step_definitions/webstormSupport.steps.js
@@ -0,0 +1,24 @@
+/* global given, when, then */
+
+const Given = given;
+const When = when;
+const Then = then;
+
+let counter = 0;
+
+Given(`I am using the Cypress Cucumber plugin`, () => {
+  counter += 1;
+});
+
+When(`I write my steps using caps`, () => {
+  counter += 1;
+});
+
+Then(/^Webstorm recognizes my (\d+) steps$/, value => {
+  counter += 1;
+  expect(counter).to.equal(value);
+});
+
+Then(/^Webstorm recognizes "(.*?)"$/, value => {
+  expect("words").to.equal(value);
+});
