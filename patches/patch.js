708557b31f9bc273ce5e1f498e45466e634bed30
Matthew Stern <matt.st97@gmail.com>
2024-11-04 17:41:13 -0500

    refactor patch-package fix to not depend on directory structure

--- patches/patch.js
+++ patches/patch.js
@@ -4,12 +4,16 @@
  * The intention here is to only run `patch-package` during local installation, IE. during
  * development of the library and not upon installation by the users of the library.
  */
-const fs = require('fs');
-const path = require('path');
 
-const isInstallingAsDependency = fs.existsSync(path.join(process.cwd(), 'package.json')) &&
-  !fs.existsSync(path.join(process.cwd(), 'src'));
+// Check if the script is running within node_modules
+const isInNodeModules = __dirname.includes('node_modules');
 
-if (!isInstallingAsDependency) {
+// Check if the script is running in a workspace (e.g., pnpm, yarn workspaces)
+const isInWorkspace = process.env.PWD && process.env.PWD.includes('node_modules');
+
+// Determine if we should run patch-package
+const shouldRunPatchPackage = !isInNodeModules && !isInWorkspace;
+
+if (shouldRunPatchPackage) {
   require('patch-package');
 }
